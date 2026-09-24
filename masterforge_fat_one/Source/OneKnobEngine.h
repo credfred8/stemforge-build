332fd7b8fd7772b07b374162c94981cac989b53d
credfred8 <credfred8@gmail.com>
2026-09-24 13:14:02 +0300

    Repair JUCE include line break

--- masterforge_fat_one/Source/OneKnobEngine.h
+++ masterforge_fat_one/Source/OneKnobEngine.h
@@ -1,5 +1,6 @@
 #pragma once
-#include <juce_audio_basics/juce_audio_basics.h>\n#include <juce_dsp/juce_dsp.h>
+#include <juce_audio_basics/juce_audio_basics.h>
+#include <juce_dsp/juce_dsp.h>
 #include <cmath>
 
 class OneKnobEngine
